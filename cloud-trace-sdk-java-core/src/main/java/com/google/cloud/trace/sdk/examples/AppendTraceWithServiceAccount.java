cd116ebe35c97a511db192420369c359b723b1df
scottmcmaster <smcmaster@google.com>
2015-04-22 17:36:55 -0700

    Multi-module ALL the things! And add a request factory that understands URLFetch for GAE
    Change-Id: I90abe237237385f59d42537e89beb974af5366b8

---
+++ cloud-trace-sdk-java-core/src/main/java/com/google/cloud/trace/sdk/examples/AppendTraceWithServiceAccount.java
@@ -0,0 +1,77 @@
+// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.cloud.trace.sdk.examples;
+
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.apache.ApacheHttpTransport;
+import com.google.cloud.trace.sdk.HttpTransportCloudTraceRequestFactory;
+import com.google.cloud.trace.sdk.CloudTraceWriter;
+import com.google.cloud.trace.sdk.ServiceAccountCredentialProvider;
+import com.google.cloud.trace.sdk.TraceSpanData;
+import com.google.cloud.trace.sdk.UUIDTraceIdGenerator;
+
+import java.io.File;
+import java.math.BigInteger;
+
+/**
+ * Writes a trace to the Cloud Trace API using an OAuth2 service account.
+ * Command-line arguments:
+ *   0: The email address of the service account in the Cloud project.
+ *   1: Full path to a service account P12 key file.
+ *   2: The Cloud Trace API endpoint.
+ *   3. The project ID to write the trace to.
+ *   4. The trace ID.
+ */
+public class AppendTraceWithServiceAccount {
+
+  public static void main(String[] args) throws Exception {
+    ServiceAccountCredentialProvider credentialProvider = new ServiceAccountCredentialProvider();
+    credentialProvider.setEmailAddress(args[0]);
+    credentialProvider.setP12File(new File(args[1]));
+
+    CloudTraceWriter writer = new CloudTraceWriter();
+    HttpTransport httpTransport = new ApacheHttpTransport();
+    HttpRequestFactory requestFactory = httpTransport.createRequestFactory(
+        credentialProvider.getCredential(CloudTraceWriter.SCOPES));
+    writer.setRequestFactory(new HttpTransportCloudTraceRequestFactory(httpTransport, requestFactory));
+    writer.setApiEndpoint(args[2]);
+    writer.setProjectId(args[3]);
+    
+    TraceSpanData parentSpan = createRandomParentSpan();
+    Thread.sleep(1000);
+    
+    TraceSpanData childSpan1 = parentSpan.createChildSpanData("/CHILD1");
+    Thread.sleep(2000);
+
+    TraceSpanData childSpan2 = parentSpan.createChildSpanData("/CHILD2");
+    Thread.sleep(2000);
+
+    childSpan2.close();
+    Thread.sleep(2000);
+
+    childSpan1.close();
+    Thread.sleep(2000);
+    parentSpan.close();
+    
+    System.out.println("Writing trace span with trace id " + parentSpan.getTraceId());
+    writer.writeSpans(parentSpan, childSpan1, childSpan2);
+  }
+
+  private static TraceSpanData createRandomParentSpan() {
+    String traceId = new UUIDTraceIdGenerator().generate();
+    return new TraceSpanData(traceId, "/PARENT", BigInteger.ZERO, true);
+  }
+}
---
+++ cloud-trace-sdk-java-servlet/src/main/java/com/google/cloud/trace/sdk/servlet/TraceFilter.java
@@ -0,0 +1,87 @@
+// Copyright 2014 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.cloud.trace.sdk.servlet;
+
+import com.google.cloud.trace.sdk.LoggingTraceWriter;
+import com.google.cloud.trace.sdk.TraceSpanDataHandle;
+import com.google.cloud.trace.sdk.TraceWriter;
+import com.google.cloud.trace.sdk.ReflectionUtils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet filter that instantiates a {@link TraceWriter} and installs a {@link TraceSpanDataHandle}
+ * in the request context and a corresponding ThreadLocal.
+ */
+public class TraceFilter implements Filter {
+
+  private TraceWriter writer = new LoggingTraceWriter();
+  private TraceRequestUtils requestUtils = new TraceRequestUtils();
+  private TraceResponseUtils responseUtils = new TraceResponseUtils();
+
+  @Override
+  public void destroy() {}
+
+  /**
+   * Filter method to install a new {@link TraceSpanDataHandle} as a request attribute.
+   */
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletRequest request = (HttpServletRequest) req;
+    HttpServletResponse response = (HttpServletResponse) resp;
+    try (TraceSpanDataHandle spanDataHandle =
+        requestUtils.createRequestSpanData(writer, request)) {
+      try {
+        chain.doFilter(req, resp);        
+      } catch (Exception e) {
+        responseUtils.closeResponseSpanData(spanDataHandle.getSpanData(), response);
+      }
+    }
+  }
+
+  /**
+   * Initializes a {@link TraceWriter} if requested by the properties file init param.
+   */
+  @Override
+  public void init(FilterConfig config) throws ServletException {
+    String propertiesFileName = config.getInitParameter("trace-properties-file");
+    if (propertiesFileName != null) {
+      Properties props = new Properties();
+      try {
+        props.load(getClass().getClassLoader().getResourceAsStream(propertiesFileName));
+
+        String traceWriterClassName = props.getProperty(getClass().getName() + ".traceWriter");
+        if (traceWriterClassName != null && !traceWriterClassName.isEmpty()) {
+          writer = (TraceWriter) ReflectionUtils.createFromProperties(traceWriterClassName, props);
+        }
+
+        requestUtils.initFromProperties(props);
+      } catch (IOException e) {
+        throw new ServletException(e);
+      }
+    }
+  }
+}
