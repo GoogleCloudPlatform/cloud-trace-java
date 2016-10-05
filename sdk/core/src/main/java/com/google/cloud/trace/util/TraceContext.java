a84dc5ba90ab7831ecf5729ff66d70ac55bd51cc
Ryan Niemocienski <niemo@google.com>
2016-10-05 09:05:13 -0700

    Add method to turn TraceContext into HTTP header

--- sdk/core/src/main/java/com/google/cloud/trace/util/TraceContext.java
+++ sdk/core/src/main/java/com/google/cloud/trace/util/TraceContext.java
@@ -75,6 +75,23 @@ public class TraceContext {
   }
 
   /**
+   * Transforms this context into an HTTP request header.
+   *
+   * @return this context as a header.
+   */
+  public String toHeader() {
+    StringBuilder builder =
+        new StringBuilder()
+            .append(traceId.getTraceId().toString(16))
+            .append('/')
+            .append(spanId.getSpanId())
+            .append(";o=")
+            .append(traceOptions.getOptionsMask());
+
+    return builder.toString();
+  }
+
+  /**
    * Returns the trace identifier.
    *
    * @return the trace identifier.
